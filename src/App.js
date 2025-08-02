20b1e2a7c1e24f5b49e3dfd5e3e80acbeddadfc2
Krishna Gupta <gkrish3108@gmail.com>
2025-08-02 16:39:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,27 +11,33 @@ import './App.css'
 
 export function App(){
 
+  // this codes stores the people and expenses in the local browser so that even if page reloads it doesnt disappear
+
+  // to do so we useState() with localstorage
   const [people, setPeople] = useState(()=>{
-    const savedPeople = localStorage.getItem('people');
-  return savedPeople ? JSON.parse(savedPeople) : [];
+    const savedPeople = localStorage.getItem('people'); //this is used to get the data from the local storage
+  return savedPeople ? JSON.parse(savedPeople) : []; //if there is data then convert and return in form of array otherwise
   });
 
   const [expenses, setExpenses] = useState(()=>{
     const savedExpenses = localStorage.getItem('expenses');
   return savedExpenses ? JSON.parse(savedExpenses) : [];
   });
+
+  useEffect(() => {
+  localStorage.setItem('people', JSON.stringify(people)); //our value is in form of array and localStorage can only store string hence to setitem we are using stringyfy()
+  }, [people]); //using dependancy so that it can render every time when value changes;
+  // now with the help of useEffect() we are updating the state every time the value changes
+
+  useEffect(() => {
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  }, [expenses]);
   
+  // since state is in false state non of these will visible
   const [showNetSettle, setNetSettle] = useState(false);
   const [showOptimized, setOptimized] = useState(false);
   const [showExpList, setExpList] = useState(false);
 
-  useEffect(() => {
-  localStorage.setItem('people', JSON.stringify(people));
-}, [people]);
-
-useEffect(() => {
-  localStorage.setItem('expenses', JSON.stringify(expenses));
-}, [expenses]);
 
   return<>
    
@@ -46,7 +52,7 @@ useEffect(() => {
         <div className='my-3 d-flex flex-column w-50 gap-3'>
           <button
             className={`btn ${showExpList ? 'btn-primary' : 'btn-outline-primary'} `}
-            onClick={()=>setExpList(prev=>!prev)} //this button toggle the value of showExpList
+            onClick={()=>setExpList(!showExpList)} //this button toggle the value of showExpList
           >
             {showExpList ? 'Hide Expenses List': 'Show Expenses List'}
           </button>
@@ -62,7 +68,7 @@ useEffect(() => {
           <button
             // className="btn btn-outline-primary"
             className={`btn ${showNetSettle ? 'btn-primary' : 'btn-outline-primary'}`}
-            onClick={() => setNetSettle(prev=>!prev)} //onclick false to true
+            onClick={() => setNetSettle(!showNetSettle)} //onclick false to true
           >
             {showNetSettle ? 'Hide Net Settlements' : 'Show Net Settlements '}
           </button>
@@ -78,7 +84,7 @@ useEffect(() => {
           <button
             // className="btn btn-outline-primary"
             className={`btn ${showOptimized ? 'btn-primary' : 'btn-outline-primary'}`}
-            onClick={() => setOptimized(prev=>!prev)}
+            onClick={() => setOptimized(!showOptimized)}
           >
             {showOptimized ? 'Hide Optimized Settlements' : 'Show Optimized Settlements'}
           </button>
